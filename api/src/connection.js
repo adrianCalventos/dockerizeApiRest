340021b43f1bd2ae3e110938a501f62c4a05866f
adriancalventos <adriancalventosgarcia@gmail.com>
2021-02-23 22:48:10 +0100

    add mongodb connection

--- api/src/connection.js
+++ api/src/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const User = require("./User.model");
 
-const connection = "mongodb://mongo:27017/mongo-test";
+const connection = "mongodb+srv://admin:admin@cluster0.qwixi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const connectDb = () => {
   return mongoose.connect(connection);
